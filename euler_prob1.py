f02e2ee29c378cd8e8269cb12fd89a5b47466826
wiedld <dlw405@gmail.com>
2015-05-22 16:23:05 -0700

    code cleanup

--- euler_prob1.py
+++ euler_prob1.py
@@ -1,17 +1,4 @@
 
-# def find_sum_multiples_3_5(n):
-#     """under <n, get all 3,5 multiples. sum"""
-#     total = 0
-#     for num in range(1,n):
-#         if (num%5 == 0) or (num%3 == 0):
-#             total = total + num
-#     return total
-
-# print (find_sum_multiples_3_5(10) == 23)
-# print (find_sum_multiples_3_5(20) == 78)
-# print (find_sum_multiples_3_5(30) == 195)
-
-# print find_sum_multiples_3_5(1000)
 
 
 # re-done in list comprehension
